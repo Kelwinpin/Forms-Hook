c18a5cb609f545c250a78e58a5ef5e454f737746
Kelwin.pin <Kelwincomw@gmail.com>
2023-04-18 11:21:48 -0300

    Add first validation with zod

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
 import './styles/global.css'
 
 type FormData = {
@@ -9,11 +11,18 @@ type FormData = {
     senha: string;
 }
 
-
+const createUserFormSchema = z.object({
+    nome: z.string().nonempty("Este campo é obrigatório!"),
+    sobrenome: z.string().nonempty("Este campo é obrigatório!"),
+    email:z.string().nonempty("Este campo é obrigatório!"),
+    senha:z.string().min(6, "A senha precisa de no mínimo 6 caracteres")
+})
 
 export default function Form(){
     const [output, setOutput] = useState('');
-    const {reset, handleSubmit, register} = useForm<FormData>();
+    const {reset, handleSubmit, register, formState:{errors}} = useForm<FormData>({
+        resolver: zodResolver(createUserFormSchema),
+    });
 
     const enviarDados = (data: FormData) =>{
         setOutput(JSON.stringify(data, null, 2))
@@ -38,21 +47,25 @@ export default function Form(){
                 className="flex-1 rounded border border-zinc-300 shadow-sm px-3 py-2 text-zinc-800 focus:outline-none focus:ring-2 focus:ring-violet-500" 
                 type="text" 
                 {...register("nome")}/>
+                {errors.nome && <span className="text-red-600 font-light text-sm">{errors.nome.message}</span>}
 
                 <label className="text-sm text-zinc-600 flex items-center justify-between">Sobrenome</label>
                 <input type="text" 
                 className="flex-1 rounded border border-zinc-300 shadow-sm px-3 py-2 text-zinc-800 focus:outline-none focus:ring-2 focus:ring-violet-500"
                 {...register("sobrenome")}/> 
+                {errors.sobrenome && <span className="text-red-600 font-light text-sm">{errors.sobrenome.message}</span>}
 
                 <label className="text-sm text-zinc-600 flex items-center justify-between">Email</label>
                 <input type="text" 
                 className="flex-1 rounded border border-zinc-300 shadow-sm px-3 py-2 text-zinc-800 focus:outline-none focus:ring-2 focus:ring-violet-500" 
                 {...register("email")}/>
+                {errors.email && <span className="text-red-600 font-light text-sm">{errors.email.message}</span>}
 
                 <label className="text-sm text-zinc-600 flex items-center justify-between">Senha</label>
                 <input type="password" 
                 className="flex-1 rounded border border-zinc-300 shadow-sm px-3 py-2 text-zinc-800 focus:outline-none focus:ring-2 focus:ring-violet-500" 
                 {...register("senha")}/>
+                {errors.senha && <span className="text-red-600 font-light text-sm">{errors.senha.message}</span>}
 
                 <div className="flex justify-center">
                     <input type="submit" className=" bg-emerald-700 hover:bg-emerald-800 rounded p-2" value="Submit"/>
