ce7a4f62a6c9d31233b9d3a4de96050c0255f5dc
Kelwin.pin <Kelwincomw@gmail.com>
2023-04-18 13:51:35 -0300

    Add validation of email and password

--- src/Form.tsx
+++ src/Form.tsx
@@ -4,23 +4,18 @@ import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
 import './styles/global.css'
 
-type FormData = {
-    nome: string;
-    sobrenome: string;
-    email: string;
-    senha: string;
-}
+type FormData = z.infer<typeof createUserFormSchema>
 
 const createUserFormSchema = z.object({
-    nome: z.string().nonempty("Este campo é obrigatório!"),
-    sobrenome: z.string().nonempty("Este campo é obrigatório!"),
-    email:z.string().nonempty("Este campo é obrigatório!"),
+    nome: z.string().nonempty("Este campo é obrigatório!").transform(nome =>{return nome[0].toLocaleUpperCase().concat(nome.substring(1))}),
+    sobrenome: z.string().nonempty("Este campo é obrigatório!").transform(nome =>{return nome[0].toLocaleUpperCase().concat(nome.substring(1))}),
+    email:z.string().nonempty("Este campo é obrigatório!").toLowerCase().endsWith('@mail.com', 'Email inválido'),
     senha:z.string().min(6, "A senha precisa de no mínimo 6 caracteres")
 })
 
 export default function Form(){
     const [output, setOutput] = useState('');
-    const {reset, handleSubmit, register, formState:{errors}} = useForm<FormData>({
+    const {reset, handleSubmit, register, formState:{errors}, watch} = useForm<FormData>({
         resolver: zodResolver(createUserFormSchema),
     });
 
@@ -33,6 +28,9 @@ export default function Form(){
             senha: ''
         })
     }
+
+    const senhaUsuario = watch('senha');
+    const isPasswordStrong = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
 
   return(
     <main className="h-screen flex flex-col gap-6 items-center justify-center bg-zinc-900">
@@ -61,11 +59,16 @@ export default function Form(){
                 {...register("email")}/>
                 {errors.email && <span className="text-red-600 font-light text-sm">{errors.email.message}</span>}
 
-                <label className="text-sm text-zinc-600 flex items-center justify-between">Senha</label>
+                <label className="text-sm text-zinc-600 flex items-center justify-between">Senha
+                {senhaUsuario === ''? <></> : 
+                isPasswordStrong.test(senhaUsuario) ? <span className="text-xs text-emerald-600">Senha forte</span> : 
+                <span className="text-xs text-red-500">Senha fraca</span>}
+                </label>
                 <input type="password" 
                 className="flex-1 rounded border border-zinc-300 shadow-sm px-3 py-2 text-zinc-800 focus:outline-none focus:ring-2 focus:ring-violet-500" 
                 {...register("senha")}/>
                 {errors.senha && <span className="text-red-600 font-light text-sm">{errors.senha.message}</span>}
+                
 
                 <div className="flex justify-center">
                     <input type="submit" className=" bg-emerald-700 hover:bg-emerald-800 rounded p-2" value="Submit"/>
